b5d9122aeb979dd10398eb2650a4ee56cd568fb4
raihan512 <raihangazi1024@gmail.com>
2023-11-13 06:51:06 +0600

    singlemeal page ingredients and measure comma issue solved

--- src/Component/SingleMeal/SingleMeal.js
+++ src/Component/SingleMeal/SingleMeal.js
@@ -15,44 +15,54 @@ const SingleMeal = () => {
     strTags,
     strYoutube,
   } = meal.meals[0];
-  console.log(strTags);
+
   const getArr = (keyword) => {
     const keywordArr = [];
     for (let i = 0; i < 21; i++) {
       let keywordItem = keyword + i;
       let keywordExist = meal.meals[0][keywordItem];
-      if (keywordExist !== " ") keywordArr.push(keywordExist);
-      else if (!keywordExist) keywordArr.push(keywordExist);
+      if (keywordExist) {
+        if (keywordExist.length > 2) {
+          keywordArr.push(keywordExist);
+        }
+      }
     }
     return keywordArr;
   };
 
   const ingredients = getArr("strIngredient");
   const measure = getArr("strMeasure");
-  console.log(meal.meals[0]);
-  console.log(measure);
+
   return (
     <div className="container mx-auto my-5">
       <h2 className="text-2xl font-bold mb-3 text-center">{strMeal}</h2>
       <div className="grid grid-cols-5 gap-10 mt-10">
         <img src={strMealThumb} className="col-span-2" alt="" />
         <div className="col-span-3">
-          <p>
-            <span className="text-lg font-semibold">Origin:</span>
-            <span className="">{strArea}</span>
+          <p className="grid grid-cols-6 mb-3">
+            <span className="text-lg font-semibold col-span-1">Origin:</span>
+            <span className="col-span-5">{strArea}</span>
           </p>
-          <p>
-            <span className="text-lg font-semibold">Category:</span>
-            <span className="">{strCategory}</span>
+          <p className="grid grid-cols-6 mb-3">
+            <span className="text-lg font-semibold col-span-1">Category:</span>
+            <span className="col-span-5">{strCategory}</span>
           </p>
-          <p>
-            <span className="text-lg font-semibold">Ingredients:</span>
-            <span>{ingredients.join(",")}</span>
+          <p className="grid grid-cols-6 mb-3">
+            <span className="text-lg font-semibold col-span-1">
+              Ingredients:
+            </span>
+            <span className="col-span-5">{ingredients.join(",")}</span>
           </p>
-          <p>
-            <span className="text-lg font-semibold">Ingredients:</span>
-            <span>{measure.join(",")}</span>
+          <p className="grid grid-cols-6 mb-3">
+            <span className="text-lg font-semibold col-span-1">Measure:</span>
+            <span className="col-span-5">{measure.join(",")}</span>
           </p>
+          {dateModified && (
+            <p className="grid grid-cols-6 mb-3">
+              <span className="text-lg font-semibold col-span-1">Updated:</span>
+              <span className="col-span-5">{dateModified}</span>
+            </p>
+          )}
           <button className="bg-[#ff0000] text-white py-2 px-12 mt-10">
             <Link to={strYoutube}>Watch Video</Link>
           </button>
